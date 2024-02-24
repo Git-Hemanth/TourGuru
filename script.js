dcfaff212a8179ce6399e44cd9ca8b5daca24860
Hemanth Bhargav Dasari <147172782+Git-Hemanth@users.noreply.github.com>
2024-02-24 19:47:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ window.onload=()=>{
         document.querySelector('.header').classList.remove('active');
     }
 };
+
+const form=document.querySelector('form');
+const email=document.getElementById('email');
+const phone=document.getElementById('phone');
+const subject=document.getElementById('subject');
+const message=document.getElementById('message');
+const Fullname=document.getElementById('name');
+
+function sendEmail(){
+    const bodyMessage=`Full Name:${Fullname.value}<br>Email:${email.value}<br> 
+    Phone Number:${phone.value}<br>Subject:${subject.value}<br>Message:${message.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "hemanthforlearning@gmail.com",
+        Password : "7DD6AA2880F7AA422BEEB938BA8C5F809D67",
+        To : 'hemanthforlearning@gmail.com',
+        From : "hemanthforlearning@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if(message=="OK"){
+            Swal.fire({
+                title: "Thank You For Choosing Us!",
+                text: "We will reach out you Soon!",
+                icon: "success"
+              });
+        }
+    });
+}
+
+    form.addEventListener("submit",(e) => {
+        e.preventDefault();
+    
+        sendEmail();
+    });
+    
